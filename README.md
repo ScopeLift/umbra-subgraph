65a2aca35c04189978da4ee26156f3a66ad63a2e
Matt Solomon <matt@mattsolomon.dev>
2021-11-12 15:29:24 -0800

    feat: initial subgraph setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Umbra Subgraph
+
+## Development
+
+1. Install packages with `yarn`
+2. Deploy with `yarn deploy:mainnet` or `yarn deploy:polygon`
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,32 +1,23 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Umbra,
-  Announcement,
-  OwnershipTransferred,
-  TokenWithdrawal
-} from "../generated/Umbra/Umbra"
-import { ExampleEntity } from "../generated/schema"
+import { } from "@graphprotocol/graph-ts"
+import { Announcement } from "../generated/Umbra/Umbra"
+import { AnnouncementEntity } from "../generated/schema"
 
 export function handleAnnouncement(event: Announcement): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = AnnouncementEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
+  // Entities only exist after they have been saved to the store; `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new AnnouncementEntity(event.transaction.from.toHex())
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
   entity.receiver = event.params.receiver
   entity.amount = event.params.amount
+  entity.token = event.params.token
+  entity.pkx = event.params.pkx
+  entity.ciphertext = event.params.ciphertext
 
   // Entities can be written to the store with `.save()`
   entity.save()
@@ -53,6 +44,4 @@ export function handleAnnouncement(event: Announcement): void {
   // - contract.tollReceiver(...)
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleTokenWithdrawal(event: TokenWithdrawal): void {}
